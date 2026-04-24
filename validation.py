9c03919877444e362cbbde523cfa56d370bbc77b
Adam Bermingham <adambermingham1@gmail.com>
2026-04-24 12:35:32 +0100

    Word validation + free standing letter check

--- validation.py
+++ validation.py
@@ -3,23 +3,82 @@ words = data.read()
 words = words.split('\n')
 
 def valid(board):
+    if single_letter(board) == False:
+        print('Free standing Letter')
+        return False
+
+    found_words = []
+
+    row = 0
+    while row < 15:
+        temp = ""
+        col = 0
+        while col < 15:
+            idx = row * 15 + col + 1
+            if isinstance(board[idx], str) and board[idx].isalpha():
+                temp += board[idx]
+            else:
+                if len(temp) > 1:
+                    found_words.append(temp)
+                temp = ""
+            col += 1
+        if len(temp) > 1:
+            found_words.append(temp)
+        row += 1
+
+    col = 0
+    while col < 15:
+        temp = ""
+        row = 0
+        while row < 15:
+            idx = row * 15 + col + 1
+            if isinstance(board[idx], str) and board[idx].isalpha():
+                temp += board[idx]
+            else:
+                if len(temp) > 1:
+                    found_words.append(temp)
+                temp = ""
+            row += 1
+        if len(temp) > 1:
+            found_words.append(temp)
+        col += 1
+
+    if not found_words:
+        return False
+
+    for w in found_words:
+        if w not in words:
+            print("Invalid:", w)
+            return False
+
+    print("Valid:", found_words)
+    return True
+
+
+def single_letter(board):
     box = 1
-    temp = ''
-    active = False
-    while (box < 226):
-        if board[box] != 0 and active == False:
-            temp = board[box]
-            active = True
-        elif board[box] != 0 and active == True:
-            temp += board[box]
-        if board[box] == 0 and active == True:
-            active = False
-            
-            if len(temp) > 1:
-                if temp not in words:
-                    print('Invalid')
-                    return False
-                else:
-                    print('Valid')
-                    return True
+    while box < 226:
+        if board[box] not in (0, 1):
+            row = (box - 1) // 15
+            col = (box - 1) % 15
+            touching = False
+
+            if row > 0:
+                if board[box - 15] not in (0, 1):
+                    touching = True
+            if row < 14:
+                if board[box + 15] not in (0, 1):
+                    touching = True
+            if col > 0:
+                if board[box - 1] not in (0, 1):
+                    touching = True
+            if col < 14:
+                if board[box + 1] not in (0, 1):
+                    touching = True
+
+            if touching == False:
+                return False
+
         box += 1
+
+    return True
